f46875b00394edcc5077466eeae7ad26deed7a73
bgoggans <goggansbg@gmail.com>
2026-04-12 21:51:47 -0600

    Basic Bullet Functionality
    The bullets fire on attack click ,but a timer for fire rate needs to be implemented still

--- Space Invader/Assets/Scripts/PlayerController.cs
+++ Space Invader/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,20 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Gun and Bullet References")]
+    [SerializeField] private GameObject bullet; //reference to the bullet prefab 
+    [SerializeField] private GameObject bulletSpawnPos1; //bullet spawn location on one gun
+    [SerializeField] private GameObject bulletSpawnPos2; //bullet spawn location on other gun
+
     private Vector2 moveInput; 
     private InputActionMap _inputs;
     private Rigidbody rb;
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked; // locks the cursor in the middle of the screen when game starts
+        Cursor.visible = false; //makes cursor invisible so the crosshair is used, use esc to see cursor again while in game testing 
         rb = GetComponent<Rigidbody>();
     }
 
@@ -25,6 +31,7 @@ public class PlayerController : MonoBehaviour
  
     private void FixedUpdate()
     {
+        // takes the rigidbdy componenet and uses the move vector 2 from the OnMove times the move speed value and applies it to the rigidbody in the form of vector 3 coordinates
         rb.linearVelocity = new Vector3(moveInput.x * moveSpeed, rb.linearVelocity.y, rb.linearVelocity.z);
     }
 
@@ -44,9 +51,25 @@ public class PlayerController : MonoBehaviour
     }
 
     
-
+    //References the move in the input action map and gets the vector 2 value from the button pressed
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>(); 
+    }
+
+    //When the attack button from input action is pressed spawn the bullet game object at predetermined locations 
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log($"bullet: {bullet}, pos1: {bulletSpawnPos1}, pos2: {bulletSpawnPos2}");
+
+            if (bullet == null) { Debug.LogError("bullet is null!"); return; }
+            if (bulletSpawnPos1 == null) { Debug.LogError("bulletSpawnPos1 is null!"); return; }
+            if (bulletSpawnPos2 == null) { Debug.LogError("bulletSpawnPos2 is null!"); return; }
+
+            Instantiate(bullet, bulletSpawnPos1.transform.position, Camera.main.transform.rotation);
+            Instantiate(bullet, bulletSpawnPos2.transform.position, Camera.main.transform.rotation);
+        }
     }
 }
