8dfcde73bed6dbbb814a3399b5b0b7fc92d00c4a
Ajeante S. <Ajeante.Sumpter@gmail.com>
2026-05-11 14:07:39 -0600

    code

--- Space Invader/Assets/Scripts/PlayerController.cs
+++ Space Invader/Assets/Scripts/PlayerController.cs
@@ -9,8 +9,8 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float FireRate = 5f;
-    private float nextFireTime = 0;
+    //[SerializeField] private float FireRate = 5f;
+    //private float nextFireTime = 0;
 
     private int numberOfLives;
     [SerializeField] private Slider livesSlider; // UI slider for lives
@@ -30,6 +30,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private AudioClip shootSound;
     [SerializeField] private AudioClip moveSound;
 
+    //AJ NEEDED FIXES
+    public float bulletSpeed;
+    public float fireRate, bulletDamage;
+    public bool isAuto;
+
+    private float timer;
+
+    private bool attackInput = false;
+
+    InputSystem_Actions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+    }
 
     void Start()
     {
@@ -45,9 +60,45 @@ public class PlayerController : MonoBehaviour
 
         Debug.Log($"Life Count: {numberOfLives}");
     }
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+
+        inputActions.Player.Attack.performed += ctx => attackInput = true;
+        inputActions.Player.Attack.canceled += ctx => attackInput = false;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Attack.performed -= ctx => attackInput = true;
+        inputActions.Player.Attack.canceled -= ctx => attackInput = false;
+
+        inputActions.Player.Disable();
+    }
 
     private void Update()
     {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (isAuto)
+        {
+            if (attackInput && timer <= 0)
+            {
+                OnAttack();
+            }
+        }
+        else
+        {
+            if (attackInput && timer <= 0)
+            {
+                OnAttack();
+                attackInput = false;
+            }
+        }
+
         Death();
     }
 
@@ -85,13 +136,11 @@ public class PlayerController : MonoBehaviour
     }
 
     //When the attack button from input action is pressed spawn the bullet game object at predetermined locations 
-    public void OnAttack(InputAction.CallbackContext context)
+    public void OnAttack()
     {
-        if (context.performed && Time.time >= nextFireTime)
-        {
-            nextFireTime = Time.time + FireRate;
+        timer = 1f;
 
-            Debug.Log($"bullet: {bullet}, pos1: {bulletSpawnPos1}, pos2: {bulletSpawnPos2}");
+        Debug.Log($"bullet: {bullet}, pos1: {bulletSpawnPos1}, pos2: {bulletSpawnPos2}");
 
             if (bullet == null) { Debug.LogError("bullet is null!"); return; }
             if (bulletSpawnPos1 == null) { Debug.LogError("bulletSpawnPos1 is null!"); return; }
@@ -103,12 +152,10 @@ public class PlayerController : MonoBehaviour
             b1.GetComponent<BulletController>().SetDirection(Camera.main.transform.forward);
             b2.GetComponent<BulletController>().SetDirection(Camera.main.transform.forward);
 
-
-
             // play shooting sound here 
             audioSource.PlayOneShot(shootSound); 
 
-        }
+        
     }
 
     private void Death()
